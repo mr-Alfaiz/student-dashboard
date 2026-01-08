ad7455fd4807965e28cfefff9e5ff7e3769aba5c
mr-Alfaiz <alfaisibrahim3@gmail.com>
2026-01-08 16:58:12 +0400

    fix appwrite ssr crash on vercel

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,16 +1,17 @@
 // lib/appwrite.ts
-
 import { Client, Account, Databases, ID, Query } from "appwrite";
 
 const client = new Client();
-
-client
+if (typeof window !== "undefined") {
+  client
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+}
 
 export const account = new Account(client);
-export const databaseId = '69493fc00015e17e6634'
-export const studentCollectionId ='student-data'
-export const database = new Databases(client)
-export { ID }
-export { Query }
+export const database = new Databases(client);
+
+export const databaseId = "69493fc00015e17e6634";
+export const studentCollectionId = "student-data";
+
+export { ID, Query };
--- src/app/component/Admin/page.tsx
+++ src/app/component/Admin/page.tsx
@@ -1,4 +1,7 @@
-'use client'
+"use client";
+
+export const dynamic = "force-dynamic";
+
 
 import { House } from "lucide-react"
 import { useState } from 'react'
@@ -7,69 +10,69 @@ import { account } from '@/lib/appwrite'
 import Link from "next/link"
 
 const Admin = () => {
-  const router = useRouter()
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+    const router = useRouter()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const login = async () => {
-    try {
-      await account.createEmailPasswordSession(email, password)
+    const login = async () => {
+        try {
+            await account.createEmailPasswordSession(email, password)
 
-      const user = await account.get()
+            const user = await account.get()
 
-      if (
-        user.email === 'alfaisibrahim3@gmail.com' ||
-        user.email === 'brahim3@gmail.com'
-      ) {
-        alert("Welcome")
-        router.push('/component/Admin/Dashboard')
-      } else {
-        router.push('/')
-      }
-    } catch (error) {
-      console.error(error)
-      alert("Invalid login")
+            if (
+                user.email === 'alfaisibrahim3@gmail.com' ||
+                user.email === 'brahim3@gmail.com'
+            ) {
+                alert("Welcome")
+                router.push('/component/Admin/Dashboard')
+            } else {
+                router.push('/')
+            }
+        } catch (error) {
+            console.error(error)
+            alert("Invalid login")
+        }
     }
-  }
 
-  return (
-    <div>
-      <div className="p-4 text-sky-600">
-        <Link href="../">
-          <House />
-        </Link>
-      </div>
+    return (
+        <div>
+            <div className="p-4 text-sky-600">
+                <Link href="../">
+                    <House />
+                </Link>
+            </div>
 
-      <div className='p-30'>
-        <div className="h-63 w-90 shadow-2xl mx-auto">
-          <h1 className='text-center p-4 text-2xl font-semibold text-sky-600'>Admin</h1>
+            <div className='p-30'>
+                <div className="h-63 w-90 shadow-2xl mx-auto">
+                    <h1 className='text-center p-4 text-2xl font-semibold text-sky-600'>Admin</h1>
 
-          <div className='ml-5'>
-            <label className='text-sky-600'>Email</label>
-            <input
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-80 mb-2 h-8 rounded-sm border border-gray-500"
-            />
+                    <div className='ml-5'>
+                        <label className='text-sky-600'>Email</label>
+                        <input
+                            type="email"
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-80 mb-2 h-8 rounded-sm border border-gray-500"
+                        />
 
-            <label className='text-sky-600'>Password</label>
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-80 h-8 mb-3 rounded-sm border border-gray-500"
-            />
+                        <label className='text-sky-600'>Password</label>
+                        <input
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-80 h-8 mb-3 rounded-sm border border-gray-500"
+                        />
 
-            <button
-              onClick={login}
-              className="w-80 h-9 bg-sky-600 text-white rounded-sm"
-            >
-              Login
-            </button>
-          </div>
+                        <button
+                            onClick={login}
+                            className="w-80 h-9 bg-sky-600 text-white rounded-sm"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Admin
